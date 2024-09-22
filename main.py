31d809e3b2104c2a4ef49bae4b02bbe0fcf31d04
Cyriac John <76932582+cyriacjohn@users.noreply.github.com>
2024-09-22 19:18:13 +0530

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This entrypoint file to be used in development. Start by reading README.md
+# This entrypoint file was used in development. 
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
@@ -11,13 +11,10 @@ from unittest import main
 
 
 
-# Uncomment line below to play interactively against a bot:
+
 # play(human, abbey, 20, verbose=True)
 
-# Uncomment line below to play against a bot that plays randomly:
 # play(human, random_player, 1000)
 
 
-
-# Uncomment line below to run unit tests automatically
 main(module='test_module', exit=False)
